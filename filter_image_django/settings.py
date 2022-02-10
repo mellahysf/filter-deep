b2216d9791bf6bd42bc1520eb5eec62d730b69f0
Youssef MELLAH <ysfmell@gmail.com>
2022-02-10 19:49:04 +0100

    update Readme commit

--- filter_image_django/settings.py
+++ filter_image_django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@mmld2-eas2@40piv)s6mh_#d4wf67#4k(useo1f@lvzabu#3('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["filter-image-django-mellahysf.cloud.okteto.net", "127.0.0.1", "10.0.2.2"]
 
