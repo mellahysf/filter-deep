1c4af661f6f27ac3989f4d64a84c812bb94c5548
Youssef MELLAH <ysfmell@gmail.com>
2022-02-10 19:12:26 +0100

    update Readme commit

--- api/models.py
+++ api/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class FilterImg(models.Model):
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="media/images/")
