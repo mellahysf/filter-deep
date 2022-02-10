185a8908f1e9f40c5242565b1d31f6822453b6bd
Youssef MELLAH <ysfmell@gmail.com>
2022-02-10 18:24:59 +0100

    update Readme commit

--- filter_image_django/settings.py
+++ filter_image_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@mmld2-eas2@40piv)s6mh_#d4wf67#4k(useo1f@lvzabu#3(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["filter1-project.herokuapp.com", "127.0.0.1", "10.0.2.2"]
+ALLOWED_HOSTS = ["filter-image-django-mellahysf.cloud.okteto.net", "127.0.0.1", "10.0.2.2"]
 
 
 # Application definition
