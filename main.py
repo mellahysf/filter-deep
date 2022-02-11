2cfb2d4e9bf016d24324aedee66c77894bbd5ee9
Youssef MELLAH <ysfmell@gmail.com>
2022-02-11 20:23:25 +0100

    commm

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ def upload():
         imagefile = request.files['file']
         filename = werkzeug.utils.secure_filename(imagefile.filename)
         print("\nReceived image File name : " + imagefile.filename)
-        imagefile.save("media/inputs/upload/" + filename)
+        imagefile.save("./media/inputs/upload/" + filename)
         ## TODO: Call model and get res_image
         ##!python inference_gfpgan.py --upscale 2 --test_path inputs/upload --save_root results --model_path experiments/pretrained_models/GFPGANCleanv1-NoCE-C2.pth --bg_upsampler realesrgan
-        save_restore_path = inference("media/inputs/upload/" + filename)
-        os.remove("media/inputs/upload/"+filename)
+        save_restore_path = inference("./media/inputs/upload/" + filename)
+        os.remove("./media/inputs/upload/"+filename)
         fin = time.time()
         print("inference DONE************" + str(fin-deb))
         print("save_restore_path///////////" + save_restore_path)
